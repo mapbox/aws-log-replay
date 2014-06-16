d6de7b649f2f2bad721b2626d98020087cd231dd
Ian Ward <ianshward@gmail.com>
2014-06-16 00:54:37 -0400

    Make it a stream

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var AWS = require('aws-sdk'),
     _ = require('underscore'),
     zlib = require('zlib'),
-    util = require('util');
+    util = require('util'),
+    Readable = require('stream').Readable;
 
 var s3 = new AWS.S3();
 
@@ -10,77 +11,87 @@ module.exports = Reader;
 function Reader(options) {
     this.bucket = options.bucket;
     this.prefix = options.prefix;
-    this.paths = [];
     this.logs = [];
-    this.fetching = false;
+    this.fetching = 0;
+    Readable.call(this, {
+        objectMode: true
+    });
     return this;
 }
+util.inherits(Reader, Readable);
 
-Reader.prototype.read = function(cb) {
-    var called = false;
+Reader.prototype._read = function() {
     var that = this;
-    if (this.paths.length) {
-        called = true;
-        cb(null, this.paths.shift());
-    }
-    // keep at least 10000 paths in the queue
-    if (this.paths.length < 10000 && !this.fetching) {
-        this.fetching = true;
-        this._fetch(function() {
-            if (!called) return cb(null, that.paths.shift());
-        });
-    }
 
-    if (!this.paths.length) return this._wait(cb);
-};
-
-Reader.prototype._fetch = function(cb) {
-    var that = this;
-    this._list(function() {
-        if (!that.logs.length) return;
-        s3.getObject({Bucket: that.bucket, Key: that.logs.pop()}, function(err, data) {
+    var fetchAndProcess = function() {
+        that._fetch(that._pick(), function(err, data) {
             if (err) throw err;
-            zlib.gunzip(data.Body, function(err, buf) {
-                if (err) throw err;
-                var lines = buf.toString();
-                // slice removes two header lines
-                lines = lines.split('\n').slice(2);
+            that._process(data, function(err, lines) {
                 lines.forEach(function(line) {
-                    var parts = line.split(/\s+/g);
-                    if (parts.length > 7) {
-                        that.paths.push(parts[7]);
-                    }
+                    that.push(line);
                 });
-                this.fetching = false;
-                cb();
             });
         });
-    });
+    };
+
+    var prime = function() {
+        for (var i = that.fetching; i < 5; i++)
+            fetchAndProcess();
+    };
+
+    // Need to determine list of keys to fetch from S3
+    if (!this.logs.length) {
+        that._list(function(err) {
+            if (err) throw err;
+            if (that.fetching > 5) return that;
+            prime();
+        });
+    } else {
+        if (that.fetching > 5) return that;
+        prime();
+    }
 };
 
+// Fetch list of log files from S3
 Reader.prototype._list = function(cb) {
-    if (this.logs.length) return cb();
     var that = this;
     s3.listObjects({Bucket: this.bucket, Prefix: this.prefix}, function(err, data) {
-        if (err) throw err;
+        if (err) return cb(err);
         that.logs = _(data.Contents).pluck('Key');
         cb();
     });
 };
 
-Reader.prototype._wait = function(cb) {
+// Fetch log file from S3
+Reader.prototype._fetch = function(key, cb) {
+console.log('_fetch');
     var that = this;
-    var wait = function() {
-        if (that.paths.length) return cb(null, that.paths.shift());
-        else {
-            setTimeout(function() {
-                wait();
-            }, 100);
-        }
-    };
+    this.fetching++;
+    s3.getObject({Bucket: this.bucket, Key: key}, function(err, data) {
+        if (err) return cb(err);
+        that.fetching--;
+        cb(null, data.Body);
+    });
+};
 
-    if (this.paths.length) return cb(null, this.paths.shift());
-    else {
-        wait();
-    }
+// Uncompress log file, return array of urls
+Reader.prototype._process = function(file, cb) {
+    var entries = [];
+    zlib.gunzip(file, function(err, buf) {
+        if (err) return cb(err);
+        var lines = buf.toString();
+        // slice removes two header lines
+        lines = lines.split('\n').slice(2);
+        lines.forEach(function(line) {
+            var parts = line.split(/\s+/g);
+            if (parts.length > 7) {
+                entries.push(parts[7]);
+            }
+        });
+        cb(null, entries);
+    });
+};
+
+Reader.prototype._pick = function() {
+    return this.logs[this.logs.length * Math.random | 0];
 };

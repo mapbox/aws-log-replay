84fe7df6e1634337d499ecaa8ff279d8fe7fed49
Young Hahn <young@mapbox.com>
2015-10-20 00:00:06 -0400

    Sketch out remainder of module.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+var request = require('requestretry');
 var s3scan = require('s3scan');
+var split = require('split');
 var zlib = require('zlib');
+var url = require('url');
 var stream = require('stream');
+var queue = require('queue-async');
 
 module.exports = {};
 module.exports.LogStream = LogStream;
 module.exports.ScanGunzip = ScanGunzip;
+module.exports.RequestStream = RequestStream;
+module.exports.getPath = getPath;
 
 function LogStream(uri, options) {
     options = options || {};
@@ -15,7 +21,7 @@ function LogStream(uri, options) {
         .on('error', function(err) {
             scanGunzip.emit('error', err);
         });
-    scan.pipe(scanGunzip);
+    scan.pipe(scanGunzip).pipe(split());
     return scanGunzip;
 }
 
@@ -29,5 +35,68 @@ function ScanGunzip() {
         });
     };
     return scanGunzip;
+}
+
+function RequestStream(options) {
+    options = options || {};
+    if (!options.baseurl) throw new Error('options.baseurl should be an http:// or https:// baseurl for replay requests');
+
+    var starttime = Date.now();
+    var requestStream = new stream.Transform(options);
+    requestStream._readableState.objectMode = true;
+    requestStream.got = 0;
+    requestStream.pending = 0;
+    requestStream.queue = queue();
+    requestStream.queue.awaitAll(function(err) {
+        if (err) requestStream.emit('error', err);
+    });
+
+    requestStream._transform = function(line, enc, callback) {
+        if (typeof line !== 'string') line = line.toString('utf8');
+
+        var pathname = getPath(line);
+
+        if (!pathname) return callback();
+
+        var uri = url.parse(pathname, true);
+
+        if (requestStream.pending > 1000) {
+            return setImmediate(requestStream._transform.bind(requestStream), line, enc, callback);
+        }
+
+        requestStream.pending++;
+        requestStream.queue.defer(function(next) {
+            request({
+                agent: options.agent,
+                encoding: null,
+                uri: options.baseurl + url.format(uri),
+            }, function(err, res, body) {
+                requestStream.pending--;
+                if (err) return requestStream.emit('error');
+                if (res.statusCode !== 200) return next();
+                requestStream.push({ Body: body });
+                requestStream.got++;
+                next();
+            });
+        });
+        callback();
+    };
+
+    requestStream._flush = function(callback) {
+        requestStream.queue.awaitAll(callback);
+    };
+
+    return requestStream;
+}
+
+function getPath(line) {
+    var parts = line.split(/\s+/g);
+    if (parts.length > 7) {
+        if (parts[11] !== "-") {
+            return parts[7] + "?" + parts[11];
+        } else {
+            return parts[7];
+        }
+    }
 }
 
---
+++ test/RequestStream.test.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var reader = require('../index.js');
+var tape = require('tape');
+var http = require('http');
+var server;
+
+tape('setup', function(assert) {
+    server = http.createServer(function(req, res) {
+        if (/\/c\.json/.test(req.url)) {
+            res.writeHead(404);
+        } else {
+            res.writeHead(200);
+            res.write(JSON.stringify({ obj: req.url.split('/')[1].split('.')[0] }));
+        }
+        res.end();
+    });
+    server.listen(9999, assert.end);
+});
+
+tape('RequestStream', function(assert) {
+    var data = [];
+    var reqstream = reader.RequestStream({
+        baseurl: 'http://localhost:9999'
+    });
+    reqstream.on('data', function(d) {
+        assert.deepEqual(Buffer.isBuffer(d.Body), true, 'data.Body is buffer');
+        data.push(JSON.parse(d.Body));
+    });
+    reqstream.on('end', function() {
+        assert.deepEqual(data.length, 2, 'emits 2 objects');
+        assert.deepEqual(data.map(function(d) { return d.obj }).sort(), ['a', 'b'], 'emits objects a, b');
+        assert.end();
+    });
+    reqstream.write('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	/a.json	200	https://www.mapbox.com/	FakeAgent	option=1	-	Miss	FAKE==	example.com	http	784	0.314\n');
+    reqstream.write('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	/b.json	200	https://www.mapbox.com/	FakeAgent	option=2	-	Miss	FAKE==	example.com	http	784	0.314\n');
+    reqstream.write('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	/c.json	200	https://www.mapbox.com/	FakeAgent	option=2	-	Miss	FAKE==	example.com	http	784	0.314\n');
+    reqstream.write('\n');
+    reqstream.end();
+});
+
+tape('teardown', function(assert) {
+    server.close(assert.end);
+});
+

aa581d97d23c4e87a0dcf9041804f91277677c1f
Young Hahn <young@mapbox.com>
2015-10-25 11:44:51 -0400

    Additional sanity check on path value.

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ function RequestStream(options) {
  */
 function getPath(line) {
     var parts = line.split(/\s+/g);
-    if (parts.length > 7) {
+    if (parts.length > 7 && parts[7][0] === '/') {
         if (parts[11] && parts[11] !== "-") {
             return parts[7] + "?" + parts[11];
         } else {
--- test/getPath.test.js
+++ test/getPath.test.js
@@ -4,6 +4,7 @@ var tape = require('tape');
 
 tape('getPath', function(assert) {
     assert.deepEqual(reader.getPath('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	/a.json	200	https://www.mapbox.com/	FakeAgent	option=1	-	Miss	FAKE==	example.com	http	784	0.314\n'), '/a.json?option=1');
+    assert.deepEqual(reader.getPath('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	bogusPath	200	https://www.mapbox.com/	FakeAgent	option=1	-	Miss	FAKE==	example.com	http	784	0.314\n'), undefined);
     assert.deepEqual(reader.getPath('\n'), undefined);
     assert.end();
 });

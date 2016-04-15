1acc9c6e50b4a8df85d19cc169dca425e81a8282
Tom Lee <thomas.j.lee@gmail.com>
2016-04-14 22:50:47 -0400

    simplify test interface

--- test/SampleStream.test.js
+++ test/SampleStream.test.js
@@ -12,7 +12,7 @@ function testFunc(r, f, expected, t) {
         count++;
     });
     sample.on('finish', function() {
-        t.equals(count, expected[r - 1], 'got expected records (' + expected[r - 1] + ')');
+        t.equals(count, expected, 'got expected records (' + expected + ')');
         t.end();
     });
 
@@ -31,11 +31,11 @@ function testFunc(r, f, expected, t) {
 
 var expectedUnfiltered = [949, 1879, 2820, 3728, 4592, 5449, 6323, 7220, 8136];
 for (var rate = 1; rate < 10; rate++) {
-    tape('unfiltered, sample rate ' + (rate * 0.1).toFixed(1), testFunc.bind(null, rate, false, expectedUnfiltered));
+    tape('unfiltered, sample rate ' + (rate * 0.1).toFixed(1), testFunc.bind(null, rate, false, expectedUnfiltered[rate - 1]));
 }
 
 var expectedFiltered = [299, 607, 904, 1218, 1509, 1788, 2085, 2384, 2716];
 for (var rate = 1; rate < 10; rate++) {
-    tape('filtered, sample rate ' + (rate * 0.1).toFixed(1), testFunc.bind(null, rate, 'a\.json', expectedFiltered));
+    tape('filtered, sample rate ' + (rate * 0.1).toFixed(1), testFunc.bind(null, rate, 'a\.json', expectedFiltered[rate - 1]));
 }
 

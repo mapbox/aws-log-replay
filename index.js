92f2b20b46856d5ab07f43febbb8204f1d335963
Ryan Clark <ryan.clark.j@gmail.com>
2016-12-06 15:00:25 -0700

    eslint stick

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var request = require('requestretry');
-var s3scan = require('s3scan');
-var split = require('split');
-var zlib = require('zlib');
 var url = require('url');
 var stream = require('stream');
-var queue = require('queue-async');
 var crypto = require('crypto');
 var parallel = require('parallel-stream');
 
@@ -19,14 +15,14 @@ module.exports.SampleStream = SampleStream;
  * @param {string path} - a cloudfront path to decode
  */
 function cloudFrontDecode(path) {
-    var whitelist = ['3C', '3E', '22', '23', '25', '7B', '7D', '7C', '5C', '5E', '7E', '5B', '5D', '60', '27', '20'];
-    return path.replace(/%([\dA-F]{2})/g, function(match, hex) {
-        var code = parseInt(hex, 16);
-        if ((code < 32) || (code > 127) || (whitelist.indexOf(hex) !== -1))
-            return String.fromCharCode(code);
-        else
+  var whitelist = ['3C', '3E', '22', '23', '25', '7B', '7D', '7C', '5C', '5E', '7E', '5B', '5D', '60', '27', '20'];
+  return path.replace(/%([\dA-F]{2})/g, function(match, hex) {
+    var code = parseInt(hex, 16);
+    if ((code < 32) || (code > 127) || (whitelist.indexOf(hex) !== -1))
+      return String.fromCharCode(code);
+    else
             return match;
-    });
+  });
 }
 
 /**
@@ -35,31 +31,31 @@ function cloudFrontDecode(path) {
  * @param {string} type
  */
 function GeneratePath(type) {
-    var generatePath = new stream.Transform({ objectMode: true });
-    generatePath._transform = function(line, enc, callback) {
-        if (!line) return callback();
-        if (Buffer.isBuffer(line)) line = line.toString('utf-8');
-        if (type.toLowerCase() == 'cloudfront') {
-            var parts = line.split(/\s+/g);
-            if (parts.length > 7) {
-                if (parts[11] && parts[11] !== "-") {
-                    generatePath.push(cloudFrontDecode(parts[7] + "?" + parts[11]));
-                } else {
-                    generatePath.push(cloudFrontDecode(parts[7]));
-                }
-            }
-        } else if (type.toLowerCase() == 'lb') {
-            if (line.indexOf('Amazon Route 53 Health Check Service') > -1) return callback();
-            var parts = line.split(/\s+/g);
-            if (parts.length < 12) return callback();
-            var path = parts.length === 18 ? parts[12] : parts[13];
-            path = path.split(/:[0-9]\w+/g)[1];
-            if (!path) return callback();
-            generatePath.push(path);
+  var generatePath = new stream.Transform({ objectMode: true });
+  generatePath._transform = function(line, enc, callback) {
+    if (!line) return callback();
+    if (Buffer.isBuffer(line)) line = line.toString('utf-8');
+    if (type.toLowerCase() == 'cloudfront') {
+      var parts = line.split(/\s+/g);
+      if (parts.length > 7) {
+        if (parts[11] && parts[11] !== '-') {
+          generatePath.push(cloudFrontDecode(parts[7] + '?' + parts[11]));
+        } else {
+          generatePath.push(cloudFrontDecode(parts[7]));
         }
-        callback();
-    };
-    return generatePath;
+      }
+    } else if (type.toLowerCase() == 'lb') {
+      if (line.indexOf('Amazon Route 53 Health Check Service') > -1) return callback();
+      parts = line.split(/\s+/g);
+      if (parts.length < 12) return callback();
+      var path = parts.length === 18 ? parts[12] : parts[13];
+      path = path.split(/:[0-9]\w+/g)[1];
+      if (!path) return callback();
+      generatePath.push(path);
+    }
+    callback();
+  };
+  return generatePath;
 }
 
 /**
@@ -69,30 +65,30 @@ function GeneratePath(type) {
  * @param {string} options.baseurl - Required. An http or https url prepended to paths when making requests.
  */
 function RequestStream(options) {
-    options = options || {};
-    if (!options.baseurl) throw new Error('options.baseurl should be an http:// or https:// baseurl for replay requests');
-    if (!options.hwm) options.hwm = 100;
+  options = options || {};
+  if (!options.baseurl) throw new Error('options.baseurl should be an http:// or https:// baseurl for replay requests');
+  if (!options.hwm) options.hwm = 100;
 
-    function transform(pathname, enc, callback) {
-        if (typeof pathname !== 'string') pathname = pathname.toString('utf8');
-        if (!pathname || pathname.indexOf('/') !== 0) return callback();
+  function transform(pathname, enc, callback) {
+    if (typeof pathname !== 'string') pathname = pathname.toString('utf8');
+    if (!pathname || pathname.indexOf('/') !== 0) return callback();
 
-        var uri = url.parse(pathname, true);
-        var requrl = options.baseurl + url.format(uri);
+    var uri = url.parse(pathname, true);
+    var requrl = options.baseurl + url.format(uri);
 
-        request({
-            agent: options.agent,
-            encoding: null,
-            uri: requrl
-        }, (err, res, body) => {
-            if (err) return callback(err);
-            if (res.statusCode !== 200) return callback();
-            this.push({ url: requrl, body: body });
-            callback();
-        });
-    }
+    request({
+      agent: options.agent,
+      encoding: null,
+      uri: requrl
+    }, (err, res, body) => {
+      if (err) return callback(err);
+      if (res.statusCode !== 200) return callback();
+      this.push({ url: requrl, body: body });
+      callback();
+    });
+  }
 
-    return parallel.transform(transform, { concurrency: options.hwm, objectMode: true });
+  return parallel.transform(transform, { concurrency: options.hwm, objectMode: true });
 }
 
 /**
@@ -104,27 +100,27 @@ function RequestStream(options) {
  * @param {string} options.filter - Optional. Regex pre-filter applied to input.
  */
 function SampleStream(options) {
-    options = options || {};
-    if (!options.rate) throw new Error('must specify a sample rate (0 < sample < 1)');
-    if ((parseFloat(options.rate) <= 0) || (parseFloat(options.rate) >= 1)) throw new Error('rate must be between 0 and 1');
+  options = options || {};
+  if (!options.rate) throw new Error('must specify a sample rate (0 < sample < 1)');
+  if ((parseFloat(options.rate) <= 0) || (parseFloat(options.rate) >= 1)) throw new Error('rate must be between 0 and 1');
 
-    var sampleStream = new stream.Transform({ objectMode: true });
-    sampleStream.count = 0;
-    sampleStream.threshold = Math.round(parseFloat(options.rate) * Math.pow(2, 16));
-    if (options.filter) sampleStream.filter = new RegExp(options.filter);
-    sampleStream._transform = function(line, enc, callback) {
-        if (!line) return callback();
+  var sampleStream = new stream.Transform({ objectMode: true });
+  sampleStream.count = 0;
+  sampleStream.threshold = Math.round(parseFloat(options.rate) * Math.pow(2, 16));
+  if (options.filter) sampleStream.filter = new RegExp(options.filter);
+  sampleStream._transform = function(line, enc, callback) {
+    if (!line) return callback();
 
-        if ((sampleStream.filter) && (!sampleStream.filter.test(line)))
-            return callback();
+    if ((sampleStream.filter) && (!sampleStream.filter.test(line)))
+      return callback();
 
-        var hash = crypto.createHash('md5').update('cloudfront-log-read-salt-' + sampleStream.count).digest().readUInt16LE(0);
-        if (hash < sampleStream.threshold)
-            sampleStream.push(line);
-        sampleStream.count++;
+    var hash = crypto.createHash('md5').update('cloudfront-log-read-salt-' + sampleStream.count).digest().readUInt16LE(0);
+    if (hash < sampleStream.threshold)
+      sampleStream.push(line);
+    sampleStream.count++;
 
-        callback();
-    };
+    callback();
+  };
 
-    return sampleStream;
+  return sampleStream;
 }
--- test/GeneratePath.test.js
+++ test/GeneratePath.test.js
@@ -1,42 +1,41 @@
-var fs = require('fs');
 var reader = require('../index.js');
 var tape = require('tape');
 
 tape('GeneratePath [cloudfront]', function(assert) {
-    var data = [];
-    var generatePath = reader.GeneratePath('cloudfront');
-    generatePath.on('data', function(d) {
-        data.push(d);
-    });
-    generatePath.on('end', function() {
-        assert.equal(data[0], '/a.json?option=1');
-        assert.equal(data[1], '/geocoding/v5/mapbox.places/2401%20Gw%20Loy%20Rd%20New%20Market%2C%20Tn%2037820.json?access_token=pk.abc.123');
-        assert.equal(data.length, 2);
-        assert.end();
-    });
-    generatePath.write('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	/a.json	200	https://www.mapbox.com/	FakeAgent	option=1	-	Miss	FAKE==	example.com	http	784	0.314\n');
-    generatePath.write('2016-04-14      02:40:16        DFW50   1771    108.223.176.206 GET     d3eju24r2ptc5d.cloudfront.net   /geocoding/v5/mapbox.places/2401%2520Gw%2520Loy%2520Rd%2520New%2520Market%252C%2520Tn%252037820.json        200     -       Mozilla/5.0%2520(iPhone;%2520CPU%2520iPhone%2520OS%25209_2%2520like%2520Mac%2520OS%2520X)%2520AppleWebKit/601.1.46%2520(KHTML,%2520like%2520Gecko)%2520Mobile/13C75 access_token=pk.abc.123       -       Miss    TbuZW6a2aUgeR9kBenr30jytUpJW5tSuv20Xv3n-smuTCqSdBjGpZQ==    a.tiles.mapbox.com      http    451     1.954   -       -       -       Miss\n');
-    generatePath.write('\n');
-    generatePath.end();
+  var data = [];
+  var generatePath = reader.GeneratePath('cloudfront');
+  generatePath.on('data', function(d) {
+    data.push(d);
+  });
+  generatePath.on('end', function() {
+    assert.equal(data[0], '/a.json?option=1');
+    assert.equal(data[1], '/geocoding/v5/mapbox.places/2401%20Gw%20Loy%20Rd%20New%20Market%2C%20Tn%2037820.json?access_token=pk.abc.123');
+    assert.equal(data.length, 2);
+    assert.end();
+  });
+  generatePath.write('2014-09-05	12:48:00	IAD53	33125	54.236.254.12	GET	d3eju24r2ptc5d.cloudfront.net	/a.json	200	https://www.mapbox.com/	FakeAgent	option=1	-	Miss	FAKE==	example.com	http	784	0.314\n');
+  generatePath.write('2016-04-14      02:40:16        DFW50   1771    108.223.176.206 GET     d3eju24r2ptc5d.cloudfront.net   /geocoding/v5/mapbox.places/2401%2520Gw%2520Loy%2520Rd%2520New%2520Market%252C%2520Tn%252037820.json        200     -       Mozilla/5.0%2520(iPhone;%2520CPU%2520iPhone%2520OS%25209_2%2520like%2520Mac%2520OS%2520X)%2520AppleWebKit/601.1.46%2520(KHTML,%2520like%2520Gecko)%2520Mobile/13C75 access_token=pk.abc.123       -       Miss    TbuZW6a2aUgeR9kBenr30jytUpJW5tSuv20Xv3n-smuTCqSdBjGpZQ==    a.tiles.mapbox.com      http    451     1.954   -       -       -       Miss\n');
+  generatePath.write('\n');
+  generatePath.end();
 });
 
 tape('GeneratePath [elb]', function(assert) {
-    var data = [];
-    var generatePath = reader.GeneratePath('lb');
-    generatePath.on('data', function(d) {
-        data.push(d);
-    });
-    generatePath.on('end', function() {
-        assert.equal(data[0], '/a.json?option=1');
-        assert.equal(data[1], '/b.json?option=1&other=2');
-        assert.equal(data[2], '/ham/sam?iam');
-        assert.equal(data.length, 3);
-        assert.end();
-    });
-    generatePath.write('2016-02-01T19:04:59.488164Z eggs-VPC 000.000.000.00:00000 00.0.00.00:00 0.000024 0.006806 0.00002 304 304 0 0 "GET http://green-eggs.com:80/a.json?option=1 HTTP/1.1" "Amazon CloudFront" - -');
-    generatePath.write('2016-02-01T19:04:59.488164Z eggs-VPC 000.000.000.00:00000 00.0.00.00:00 0.000024 0.006806 0.00002 200 200 0 0 "GET http://green-eggs.com:666/b.json?option=1&other=2 HTTP/1.1" "Amazon CloudFront" - -');
-    generatePath.write('us-east-1.elb.amazonaws.com:666/ HTTP/1.1" "Amazon Route 53 Health Check Service; ref:000-000-0000; report http://green.eggs" ABCD-EFGH TLSv1.2');
-    generatePath.write('https 2016-09-25T07:15:01.253924Z app/api-green-eggs/1234 00.000.000.00:00000 00.0.0.000:00000 0.000 0.000 0.000 000 000 0000 000 "POST https://green.eggs.com:000/ham/sam?iam HTTP/1.1" "(null)/0.0.0/000 greeneggsandham/0.0" ABCDE-ABC-ABC000-ABC TLSv1 greeneggsandhamsamiam');
-    generatePath.write('\n');
-    generatePath.end();
+  var data = [];
+  var generatePath = reader.GeneratePath('lb');
+  generatePath.on('data', function(d) {
+    data.push(d);
+  });
+  generatePath.on('end', function() {
+    assert.equal(data[0], '/a.json?option=1');
+    assert.equal(data[1], '/b.json?option=1&other=2');
+    assert.equal(data[2], '/ham/sam?iam');
+    assert.equal(data.length, 3);
+    assert.end();
+  });
+  generatePath.write('2016-02-01T19:04:59.488164Z eggs-VPC 000.000.000.00:00000 00.0.00.00:00 0.000024 0.006806 0.00002 304 304 0 0 "GET http://green-eggs.com:80/a.json?option=1 HTTP/1.1" "Amazon CloudFront" - -');
+  generatePath.write('2016-02-01T19:04:59.488164Z eggs-VPC 000.000.000.00:00000 00.0.00.00:00 0.000024 0.006806 0.00002 200 200 0 0 "GET http://green-eggs.com:666/b.json?option=1&other=2 HTTP/1.1" "Amazon CloudFront" - -');
+  generatePath.write('us-east-1.elb.amazonaws.com:666/ HTTP/1.1" "Amazon Route 53 Health Check Service; ref:000-000-0000; report http://green.eggs" ABCD-EFGH TLSv1.2');
+  generatePath.write('https 2016-09-25T07:15:01.253924Z app/api-green-eggs/1234 00.000.000.00:00000 00.0.0.000:00000 0.000 0.000 0.000 000 000 0000 000 "POST https://green.eggs.com:000/ham/sam?iam HTTP/1.1" "(null)/0.0.0/000 greeneggsandham/0.0" ABCDE-ABC-ABC000-ABC TLSv1 greeneggsandhamsamiam');
+  generatePath.write('\n');
+  generatePath.end();
 });
